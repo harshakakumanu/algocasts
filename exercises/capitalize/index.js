7ae73c1a9b3414291fd20b1b4041929fd808b445
Harsha <harsha@contiq.com>
2019-12-16 20:32:59 -0800

    Capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,12 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const arr = [];
+  for(let char of str.split(' ')){
+   arr.push(char[0].toUpperCase() + char.slice(1))
+  }
+  return arr.join(' ')
+}
 
 module.exports = capitalize;
